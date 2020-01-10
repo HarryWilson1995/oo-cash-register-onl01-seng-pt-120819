d6d00eb9830f77cbe47e15574dfd5d3861aef142
Harry Wilson <harry.wilson1995a@gmail.com>
2020-01-10 16:27:30 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,11 +13,20 @@ class CashRegister
     @total 
   end 
   
-  def add_item(title, price, quantity = 1)
-    @total + (price * quantity)
-    @@items << title * quantity 
-    @last_transaction = [item, price, quantity]
-  end 
+  def add_item(title,price,quantity = 1)
+    if quantity>1
+      i=0
+      while i<quantity
+        @items << title
+        i+=1
+      end
+    else
+      @items << title
+    end
+    @total += price*quantity
+    @last_transaction_amount = @total
+    @total
+  end
   
   def apply_discount
     if employee_discount != 0
