d1c22111664cf0c65121f313cacf4747d1fe8b56
Harry Wilson <harry.wilson1995a@gmail.com>
2020-01-10 16:22:35 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,8 @@
 class CashRegister
-  attr_accessor :total
+  attr_accessor :total, :items, :last_transaction, :employee_discount
   
   @@items = []
+  @last_transaction = [] 
   
   def initialize(employee discount = nil)
     @total = 0 
@@ -14,10 +15,17 @@ class CashRegister
   def add_item(title, price, quantity = 1)
     @total + (price * quantity)
     @@items << title * quantity 
+    @last_transaction = [item, price, quantity]
   end 
   
   def apply_discount
-    
+    if employee_discount != nil 
+      discounted_total = @total * 0.8 
+      puts "After the discount, the total comes to $#{discounted_total}."
+      @total = discounted_total
+    else 
+      puts "There is no discount to apply"
+    end 
   end 
   
   def items 
@@ -25,7 +33,7 @@ class CashRegister
   end 
   
   def void_last_transaction
-    
+    @total - @last_transaction[1]
   end 
   
 end 
